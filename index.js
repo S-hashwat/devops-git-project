e3f408267138e9c01d3570d0e5d1b3ab9f2b9ff0
Shashwat <shashwat.saxena2020@vitbhopal.ac.in>
2025-04-11 17:23:55 +0530

    Update homepage message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello, DevOps World!');
+  res.end('Hello from the Homepage Feature Branch!');
 });
 
 server.listen(3000, () => {
